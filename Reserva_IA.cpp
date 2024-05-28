0150e15e524c85e5e795008d9f2ee12d5e698d2e
carlosguariglia <carlosguariglia@gmail.com>
2024-05-28 16:51:40 -0300

    version casi final. revisar que errores de usuario pueden quedar. y ver que otras funciones se pueden agregar

--- Reserva_IA.cpp
+++ Reserva_IA.cpp
@@ -20,14 +20,72 @@ struct Reserva {
 };
 
 // Definimos la longitud máxima del arreglo de reservas
-const int MAX = 10;
+const int MAX = 3;
 
-// Función para tomar una nueva reserva
-void tomarReserva(Reserva reservas[], int& siguienteIDReserva) {
+void clearConsole();
+void tomarReserva(Reserva reservas[], int &siguienteIDReserva);
+void cargarReservasDesdeArchivo(Reserva reservas[], int &siguienteIDReserva);
+void guardarReservasEnArchivo(Reserva reservas[]);
+void mostrarReservas(const Reserva reservas[]);
+void eliminarReserva(Reserva reservas[]);
+void modificarReserva(Reserva reservas[]);
+string getnombreclase(Clase clase);
+string getnombreaeropuerto(CodigoAero aeropuerto);
+
+int main() {
+    Reserva reservas[MAX];
+    int siguienteIDReserva = 1;
+    int menu;
+
+    // Cargar reservas desde el archivo reservas.txt
+    // y asigna a siguienteIDReserva la posicion libre para ingresar una nueva reserva
+    cargarReservasDesdeArchivo(reservas, siguienteIDReserva);
+    clearConsole();
+
+    do {
+        cout << "************************************************" << endl;
+        cout << "Programa de Reservas de Vuelos" << endl;
+        cout << "************************************************" << endl;
+        cout << "1. Tomar Reserva" << endl;
+        cout << "2. Mostrar Reservas" << endl;
+        cout << "3. Actualizar una Reserva" << endl;
+        cout << "4. Eliminar una Reserva" << endl;
+        cout << "0. Guardar a Archivo y Salir" << endl;
+        cout << "************************************************" << endl;
+        cin >> menu;
+
+        switch(menu) {
+            case 1:
+                tomarReserva(reservas, siguienteIDReserva);
+                break;
+            case 2:
+                mostrarReservas(reservas);
+                break;
+            case 3:
+                modificarReserva(reservas);
+                break;
+            case 4:
+                eliminarReserva(reservas);
+                break;
+            case 0:
+                guardarReservasEnArchivo(reservas);
+                break;
+            default:
+                cout << "Opción inválida. Inténtelo de nuevo.\n";
+
+        }       
+    } while (menu != 0);
+
+    return 0;
+}
+
+void tomarReserva(Reserva reservas[], int &siguienteIDReserva) {
+    clearConsole();
     for (int i = 0; i < MAX; ++i) {
-        if (reservas[i].IDReserva == -1) {
+        if (reservas[i].IDReserva == -1)
+        {
             reservas[i].IDReserva = siguienteIDReserva++;
-            
+           
             cin.ignore();
             cout << "Introduzca su DNI: ";
             getline(cin, reservas[i].dni);
@@ -41,30 +99,50 @@ void tomarReserva(Reserva reservas[], int& siguienteIDReserva) {
             int clase;
             cout << "Seleccione la clase (1. Primera, 2. Bussiness, 3. Economica): ";
             cin >> clase;
+            while (clase < 1 || clase > 3)
+            {
+                cout << "Opcion inválida. Por favor, ingrese una opción valida.\n";
+                cout << "Seleccione la clase (1. Primera, 2. Bussiness, 3. Economica): ";
+                cin >> clase;
+            }
             reservas[i].clase = static_cast<Clase>(clase);
 
             cout << "Código Aeropuerto:\n";
-            cout << "1. Bahía Blanca - BHI\n";
-            cout << "2. Buenos Aires Aeroparque - AEP\n";
-            cout << "3. Buenos Aires El Palomar - EPA\n";
-            cout << "4. Buenos Aires Ezeiza - EZE\n";
-            cout << "5. San Carlos de Bariloche - BRC\n";
-            cout << "6. San Fernando del Valle de Catamarca - CTC\n";
-            cout << "7. Comodoro Rivadavia - CRD\n";
+            cout << "1. Bahía Blanca (BHI)\n";
+            cout << "2. Buenos Aires Aeroparque (AEP)\n";
+            cout << "3. Buenos Aires El Palomar (EPA)\n";
+            cout << "4. Buenos Aires Ezeiza (EZE)\n";
+            cout << "5. San Carlos de Bariloche (BRC)\n";
+            cout << "6. San Fernando del Valle de Catamarca (CTC)\n";
+            cout << "7. Comodoro Rivadavia (CRD)\n";
 
             int opcionOrigen, opcionDestino;
             cout << "Seleccione el aeropuerto de origen (número): ";
             cin >> opcionOrigen;
+            while (opcionOrigen < 1 || opcionOrigen > 7) {
+                cout << "Opcion inválida. Por favor, ingrese una opción valida.\n";
+                cout << "Seleccione el aeropuerto de origen (número): ";
+                cin >> opcionOrigen;
+            }
             reservas[i].origen = static_cast<CodigoAero>(opcionOrigen);
 
             cout << "Seleccione el aeropuerto de destino (número): ";
             cin >> opcionDestino;
-            while (opcionOrigen == opcionDestino) {
-                cout << "El aeropuerto de destino no puede ser el mismo que el de origen.\n";
-                cout << "Introduzca el aeropuerto de destino (número): ";
+            while (opcionDestino < 1 || opcionDestino > 7)
+            {   
+                cout << "Opcion inválida. Por favor, ingrese una opción valida.\n";
+                cout << "Seleccione el aeropuerto de destino: ";
                 cin >> opcionDestino;
+            
+                // Verificar que el aeropuerto de destino no sea el mismo que el de origen
+                if (opcionOrigen == opcionDestino)
+                    {   cout << "El aeropuerto de destino no puede ser el mismo que el de origen.\n";
+                        cout << "Seleccione el aeropuerto de destino: ";
+                        cin >> opcionDestino;
+                    }   
             }
             reservas[i].destino = static_cast<CodigoAero>(opcionDestino);
+
             break;
         }
     }
@@ -136,6 +214,7 @@ void guardarReservasEnArchivo(Reserva reservas[]) {
 }
 
 // Función para mostrar las reservas
+//TODO hacer las funciones de getnombre para imprimir los valores de los enumeradores en reserva.clase y reserva.origen y reserva.destino
 void mostrarReservas(const Reserva reservas[]) {
     cout << "Reservas actuales:\n";
     for (int i = 0; i < MAX; ++i) {
@@ -146,15 +225,51 @@ void mostrarReservas(const Reserva reservas[]) {
              << "DNI:\t\t " << reservas[i].dni << "\n"
              << "Apellido:\t " << reservas[i].apellido << "\n"
              << "Nombre:\t\t " << reservas[i].nombre << "\n"
-             << "Clase:\t\t " << reservas[i].clase << "\n"
-             << "Origen:\t\t " << reservas[i].origen << "\n"
-             << "Destino:\t " << reservas[i].destino << "\n"
+             << "Clase:\t\t " << getnombreclase(reservas[i].clase) << "\n"
+             << "Origen:\t\t " << getnombreaeropuerto(reservas[i].origen) << "\n"
+             << "Destino:\t " << getnombreaeropuerto(reservas[i].destino) << "\n"
              << "------------------------------\n";
     }
 }
 
+string getnombreclase(Clase clase) {
+    switch (clase) {
+        case PRIMERA:
+            return "Primera";
+        case BUSSINESS:
+            return "Bussiness";
+        case ECONOMICA:
+            return "Economica";
+        default:
+            return "";
+    }
+}
+
+string getnombreaeropuerto(CodigoAero aeropuerto) {
+    switch (aeropuerto) {
+        case BHI:
+            return "Bahia Blanca";
+        case AEP:
+            return "Aeroparque";
+        case EPA:
+            return "El Palomar";
+        case EZE:
+            return "Ezeiza";
+        case BRC:
+            return "Bariloche";
+        case CTC:
+            return "Catamarca";
+        case CRD:
+            return "Comodoro Rivadavia";
+        default:
+            return "";
+    }
+}
+ 
 // Función para eliminar una reserva
-void eliminarReserva(Reserva reservas[]) {
+void eliminarReserva(Reserva reservas[])
+{   
+    clearConsole();
     int id;
     cout << "Introduzca el ID de la reserva a eliminar: ";
     cin >> id;
@@ -170,97 +285,103 @@ void eliminarReserva(Reserva reservas[]) {
 }
 
 // Función para modificar una reserva
-void modificarReserva(Reserva reservas[]) {
+void modificarReserva(Reserva reservas[])
+{
     int id;
     cout << "Introduzca el ID de la reserva a modificar: ";
     cin >> id;
 
-    for (int i = 0; i < MAX; ++i) {
-        if (reservas[i].IDReserva == id) {
-            cin.ignore();
-            cout << "Introduzca su DNI: ";
-            getline(cin, reservas[i].dni);
+    for (int i = 0; i < MAX; ++i)
+    {
+        if (reservas[i].IDReserva == id)
+        {
+            cout << "Esta es la reserva que desa modificar:\n";
+            cout << "IDReserva:\t " << reservas[i].IDReserva << "\n"
+             << "DNI:\t\t " << reservas[i].dni << "\n"
+             << "Apellido:\t " << reservas[i].apellido << "\n"
+             << "Nombre:\t\t " << reservas[i].nombre << "\n"
+             << "Clase:\t\t " << reservas[i].clase << "\n"
+             << "Origen:\t\t " << reservas[i].origen << "\n"
+             << "Destino:\t " << reservas[i].destino << "\n"
+             << "------------------------------\n";
 
-            cout << "Introduzca su Apellido: ";
-            getline(cin, reservas[i].apellido);
+            cout << "Indique el dato que quiere actualizar: \n";
+            cout << "1. DNI                 2. Apellido\n";
+            cout << "3. Nombre              4. Clase\n";
+            cout << "5. Origen              6. Destino\n";  
+            cout << "7. Salir sin modificar\n";
+            int opcion;
+            cin >> opcion;
 
-            cout << "Introduzca su Nombre: ";
-            getline(cin, reservas[i].nombre);
-
-            int clase;
-            cout << "Seleccione la clase (1. Primera, 2. Bussiness, 3. Economica): ";
-            cin >> clase;
-            reservas[i].clase = static_cast<Clase>(clase);
-
-            cout << "Código Aeropuerto:\n";
-            cout << "1. Bahía Blanca - BHI\n";
-            cout << "2. Buenos Aires Aeroparque - AEP\n";
-            cout << "3. Buenos Aires El Palomar - EPA\n";
-            cout << "4. Buenos Aires Ezeiza - EZE\n";
-            cout << "5. San Carlos de Bariloche - BRC\n";
-            cout << "6. San Fernando del Valle de Catamarca - CTC\n";
-            cout << "7. Comodoro Rivadavia - CRD\n";
-
-            int opcionOrigen, opcionDestino;
-            cout << "Seleccione el aeropuerto de origen (número): ";
-            cin >> opcionOrigen;
-            reservas[i].origen = static_cast<CodigoAero>(opcionOrigen);
-
-            cout << "Seleccione el aeropuerto de destino (número): ";
-            cin >> opcionDestino;
-            while (opcionOrigen == opcionDestino) {
-                cout << "El aeropuerto de destino no puede ser el mismo que el de origen.\n";
-                cout << "Introduzca el aeropuerto de destino (número): ";
-                cin >> opcionDestino;
+            switch (opcion)
+            {
+            case 1:
+                cout << "Introduzca el nuevo DNI: ";
+                cin >> reservas[i].dni;
+                break;
+            case 2:
+                cout << "Introduzca el nuevo Apellido: ";
+                cin >> reservas[i].apellido;
+                break;
+            case 3:
+                cout << "Introduzca el nuevo Nombre: ";
+                cin >> reservas[i].nombre;
+                break;
+            case 4:
+                int clase;
+                cout << "Seleccione la nueva clase (1. Primera, 2. Bussiness, 3. Economica): ";
+                cin >> clase;
+                reservas[i].clase = static_cast<Clase>(clase);
+                break;
+            case 5:
+                int origen;
+                cout << "Código Aeropuerto:\n";
+                cout << "1. Bahía Blanca - BHI\n";
+                cout << "2. Buenos Aires Aeroparque - AEP\n";
+                cout << "3. Buenos Aires El Palomar - EPA\n";
+                cout << "4. Buenos Aires Ezeiza - EZE\n";
+                cout << "5. San Carlos de Bariloche - BRC\n";
+                cout << "6. San Fernando del Valle de Catamarca - CTC\n";
+                cout << "7. Comodoro Rivadavia - CRD\n";
+                cin >>  origen;
+                reservas[i].origen = static_cast<CodigoAero>(origen);
+                break;
+            case 6:
+                int destino;
+                cout << "Código Aeropuerto:\n";
+                cout << "1. Bahía Blanca - BHI\n";
+                cout << "2. Buenos Aires Aeroparque - AEP\n";
+                cout << "3. Buenos Aires El Palomar - EPA\n";
+                cout << "4. Buenos Aires Ezeiza - EZE\n";
+                cout << "5. San Carlos de Bariloche - BRC\n";
+                cout << "6. San Fernando del Valle de Catamarca - CTC\n";
+                cout << "7. Comodoro Rivadavia - CRD\n";
+                cin >> destino;
+                reservas[i].destino = static_cast<CodigoAero>(destino);
+                break;
+            case 7:
+                return;
+            default:
+                cout << "Opción inválida. Por favor, ingrese una opción valida (presione una tecla para continuar).\n";
+                break;
             }
-            reservas[i].destino = static_cast<CodigoAero>(opcionDestino);
-
-            cout << "Reserva modificada.\n";
+            
+            cout << "Reserva actualizada.\n";
             return;
         }
     }
     cout << "Reserva no encontrada.\n";
 }
 
-int main() {
-    Reserva reservas[MAX];
-    int siguienteIDReserva = 1;
-    int menu;
 
-    // Cargar reservas desde el archivo
-    cargarReservasDesdeArchivo(reservas, siguienteIDReserva);
-
-    do {
-        cout << "1. Tomar Reserva" << endl;
-        cout << "2. Mostrar Reservas" << endl;
-        cout << "3. Actualizar una Reserva" << endl;
-        cout << "4. Eliminar una Reserva" << endl;
-        cout << "0. Guardar a Archivo y Salir" << endl;
-        cout << "Opcion: ";
-        cin >> menu;
-
-        switch(menu) {
-            case 1:
-                tomarReserva(reservas, siguienteIDReserva);
-                break;
-            case 2:
-                mostrarReservas(reservas);
-                break;
-            case 3:
-                modificarReserva(reservas);
-                break;
-            case 4:
-                eliminarReserva(reservas);
-                break;
-            case 0:
-                guardarReservasEnArchivo(reservas);
-                break;
-            default:
-                cout << "Opción inválida. Inténtelo de nuevo.\n";
-        }
-    } while (menu != 0);
-
-    return 0;
+void clearConsole() {
+    #ifdef __unix__
+        system("clear");
+    #elif __APPLE__
+        system("clear");
+    #elif defined(_WIN32) || defined(WIN64)
+        system("cls");
+    #endif
 }
 
   //La variable siguienteIDReserva se utiliza para mantener un valor único y
